aef80765a706a43799d36111d9233717ef0e1958
razoralpha <razoralpha@gmail.com>
2020-09-12 21:17:40 +0200

    Adding another option to fix proxy https behaviour

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,17 @@
 |
 */
 
+$proxy_url    = getenv('PROXY_URL');
+$proxy_schema = getenv('PROXY_SCHEMA');
+
+if (!empty($proxy_url)) {
+	URL::forceRootUrl($proxy_url);
+}
+
+if (!empty($proxy_schema)) {
+	URL::forceSchema($proxy_schema);
+}
+
 Route::group(['middleware' => 'web'], function () {
     Route::get('/', 'WelcomeController@index');
 
